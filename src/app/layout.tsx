fa402d6784328acfd9c41003f8e5ccdcd3133923
Joshua <joshuajung14@gmail.com>
2024-11-03 21:17:48 -0600

    fixed random box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
-import PageContainer from "@/components/layout/PageContainer";
+
 
 export const metadata: Metadata = {
   title: "tigerbyte",
@@ -17,9 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-          <PageContainer>
             {children}
-          </PageContainer>
       </body>
     </html>
   );
